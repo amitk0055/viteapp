7477340e11dc3027b27c3ebb92992d0f05cc8386
unknown <amitk0055@gmail.com>
2026-02-15 15:56:47 +0530

    pre

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<string>("a");
+  const [a,b] = useState("")
 
   return (
     <>
+    
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/Navigate.tsx
@@ -0,0 +1,11 @@
+import {useState} from 'react';
+const Navigate = () => {
+    const [count, setCount] = useState<string>("a");
+  return (
+    <div>
+        <h1>Navigate</h1>   
+    </div>
+  )
+}   
+
+export default Navigate;
